e80433dd563151e687b91976f8451348beac778e
Paul RocheteauE <rocheteau.p@gmail.com>
2019-08-01 10:36:51 -0400

    Added logic to seed data & ParseWiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,30 @@ require 'pry'
 #create categories & questions starting at category#6000 via jservice
 jeopardy = Jeopardy.new
 wiki = ParseWiki.new
+  # num = 6002
+  # category_object = jeopardy.category(num)
+  # question_objects = category_object[:questions]
+  # category = Category.create!(name: category_object[:name] ,clues_count: category_object[:clues_count])
+  #   question_object = question_objects[3]
+  #   #format answer to capitals first letters of words only for improved Wikipedia queries
+  #   answer_formatted = question_object[:answer].downcase.titleize
+  #   intro = wiki.intro(answer_formatted)
+  #   image_url = wiki.image(answer_formatted)
+  #   binding.pry
+  #   Question.create!(category: category, question_text: question_object[:question_text], answer: question_object[:answer], value: question_object[:value], wiki_intro: intro, wiki_image: image_url)
+
+
 Array(6000..18410).each do |num|
-  category_object = jeopardy.category(num)
-  question_objects = category_object[:questions]
-  category = Category.create!(name: category_object[:name] ,clues_count: category_object[:clues_count])
-  question_objects.each do |question_object|
-    #format answer to capitals first letters of words only for improved Wikipedia queries
-    answer_formatted = question_object[:answer].downcase.titleize
-    intro = wiki.intro(answer_formatted)
-    image_url = wiki.image(answer_formatted)
-    Question.create!(category: category, question_text: question_object[:question_text], answer: question_object[:answer], value: question_object[:value], wiki_intro: intro, wiki_image: image_url)
+  if jeopardy.category(num) != nil
+    category_object = jeopardy.category(num)
+    question_objects = category_object[:questions]
+    category = Category.create!(name: category_object[:name] ,clues_count: category_object[:clues_count])
+    question_objects.each do |question_object|
+      #format answer to capitals first letters of words only for improved Wikipedia queries
+      answer_formatted = question_object[:answer].downcase.titleize
+      intro = wiki.intro(answer_formatted)
+      image_url = wiki.image(answer_formatted)
+      Question.create!(category: category, question_text: question_object[:question_text], answer: question_object[:answer], value: question_object[:value], wiki_intro: intro, wiki_image: image_url)
+    end
   end
 end
