595bcc3d7d6d5fa1f47d5838622a582ea4079525
Paul RocheteauE <rocheteau.p@gmail.com>
2019-08-05 18:04:59 -0400

    Changed jeopardy image name, revised game controller random count

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,7 +12,7 @@ class GamesController < ApplicationController
     @new_game = Game.create!()
     #populate Quick Game with (6) random categories to fill game board
     6.times do
-      @new_game_category = GameCategory.create!(game: @new_game, category: Category.find(rand(100)))
+      @new_game_category = GameCategory.create!(game: @new_game, category: Category.find(rand(1000)))
       #for this category, grab a random question at every question value
       [200,400,600,800,1000].each do |value|
         GameQuestion.create!(game_category: @new_game_category, question: @new_game_category.category.questions.where(value: value).sample)
