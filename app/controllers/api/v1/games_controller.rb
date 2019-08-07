4517635f700dc98e36944c521a4f32ef95ba5da7
Paul RocheteauE <rocheteau.p@gmail.com>
2019-08-07 12:42:57 -0400

    Create Fetch to update Game score

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::GamesController < ApplicationController
+skip_before_action :verify_authenticity_token
 
   def show
     render json: Game.find(params[:id])
+  end
+
+  def update
+    gameId = params[:id]
+    game = Game.find(gameId)
+    score_update = JSON.parse(request.body.read)
+    revised_score = game[:score]+score_update["value"]
+    game.update_attributes(score: revised_score)
   end
 
 end
